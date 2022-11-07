0a3d625c26bfcb7d43020e9db7a02604399016ad
Joshua Jainga <jjainga@gmail.com>
2022-11-07 11:12:13 -0800

    weight converter

---
+++ weight_converter.py
@@ -0,0 +1,11 @@
+weight = input("What is your weight?:")
+weight_unit = input("(L)bs or (K)gs?:")
+
+if weight_unit.upper() == "K":
+    new_weight = int(weight) / 0.45359237
+    print("You are " + str(round(new_weight)) + "lbs!")
+elif weight_unit.upper() =="L":
+    new_weight = int(weight) * 0.45359237
+    print("You are " + str(round(new_weight)) + "kgs!")
+
+
