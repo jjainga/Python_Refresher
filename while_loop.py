dee43f70b63d45d6b044d7e0e43eae13738a3f38
Joshua Jainga <jjainga@gmail.com>
2022-11-07 11:47:21 -0800

    guessing game with random chance

--- while_loop.py
+++ while_loop.py
@@ -11,11 +11,18 @@ print ("Done")
 
 
 def guess_game():
+    attempt = 1
     secret_number = random.randrange(1,100)
     guess = input("Guess a number from 1 to 100:")
-    while guess != secret_number:
+    while attempt < 3:
+        attempt += 1
         print(f"The secret number was {secret_number}")
         secret_number = random.randrange(1,100)
         guess = input("Guess a number from 1 to 100:")
+        if guess == secret_number:
+            print(f"You win!")
+            break
+    else:
+        print(f"Game over!")
 
 guess_game()
