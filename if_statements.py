f592050e995f06e9bb638dbafa5528f220b16b9b
Joshua Jainga <jjainga@gmail.com>
2022-11-07 09:46:36 -0800

    down payment example

---
+++ if_statements.py
@@ -0,0 +1,32 @@
+is_hot = True
+is_cold = False
+
+if is_hot:
+    print('''
+It's a hot day
+Remember to drink enough water
+    ''')
+elif is_cold:
+    print('''
+It's a cold day
+Wear some warm clothes
+    ''')
+else:
+    print("Have a lovely day!")
+
+print("Go cougs")
+
+
+# down payment excersie
+
+house_price = 1000000
+
+good_credit = False
+
+if good_credit:
+    down_payment = house_price * .1
+    print(f"{int(down_payment):,}")
+elif good_credit is not True:
+    down_payment = house_price * .2
+    print(f"{int(down_payment):,}")
+
