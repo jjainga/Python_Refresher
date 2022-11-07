08369c5b2a9f2871872311b84c6f4b7cb7def42b
Joshua Jainga <jjainga@gmail.com>
2022-11-07 12:38:06 -0800

    alert message

--- car_game.py
+++ car_game.py
@@ -3,6 +3,7 @@
 
 def car_game():
     action = ""
+    car_status = False
     while action != "QUIT":
         action = input(">").upper()
         if action == "HELP":
@@ -11,11 +12,19 @@ Try
 >start - to start the car
 >stop - to stop the car
 >quit - to end the game
-            ''')
+                ''')
         elif action == "START":
-            print(f"Car started... ready to go")
+            if car_status:
+                print(f"Car is already started...")
+            else:
+                print(f"Car started... ready to go")
+                car_status = True
         elif action == "STOP":
-            print(f"Car stopped.")
+            if not car_status:
+                print(f"Car is already stopped.")
+            else:
+                print(f"Car stopped.")
+                car_status = False
         elif action == "QUIT":
             print(f"Game ended")
             break
