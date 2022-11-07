bbc557bbff627638da2cbf49cd78fc250b0720ff
Joshua Jainga <jjainga@gmail.com>
2022-11-07 14:32:04 -0800

    shape

--- for_loop.py
+++ for_loop.py
@@ -7,3 +7,25 @@ for item in prices:
     total += item
 
 print(f"Your total is ${total}")
+
+
+for x in range(4):
+    for y in range(3):
+        print(f"({x},{y})")
+
+
+number_list = [5,2,5,2,2]
+
+for x in number_list:
+    print(f"X"* x)
+print("")
+
+def shape():
+    line = ""
+    for x in number_list:
+        line = ""
+        for y in range(x):
+            line += "X"
+        print(line)
+
+shape()           
