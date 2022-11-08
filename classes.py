0bf36f0b7a86080b5a551a6dbc1769997b4602d3
Joshua Jainga <jjainga@gmail.com>
2022-11-08 10:01:13 -0800

    trying classes

---
+++ classes.py
@@ -0,0 +1,25 @@
+#"
+# Numbers
+# Strings
+# Booleans
+# 
+# List
+# Dictionaries
+# 
+# Classes are a new type of data type and can store values and methods
+# "
+
+class Point:
+    def move(self):
+        print("move")
+
+
+    def draw(self):
+        print("draw")
+
+    
+point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.x)
+point1.draw()
