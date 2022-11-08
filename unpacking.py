0e1a147db09ee855d038c05f64f91f056130c959
Joshua Jainga <jjainga@gmail.com>
2022-11-08 08:20:47 -0800

    unpacking tuples

---
+++ unpacking.py
@@ -0,0 +1,13 @@
+coordinates = (1,2,3)
+
+coordinates[0] * coordinates[1] * coordinates[2]
+
+# or
+
+#x = coordinates[0]
+#y = coordinates[1]
+#z = coordinates[2]
+
+#or we use unpacking, we are unpacking this tuple into three variables. You can use this feature with lists as well
+
+x, y, z = coordinates
