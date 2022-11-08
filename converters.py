bffbc198289626ee4f0f1e7808c08927c117aed7
Joshua Jainga <jjainga@gmail.com>
2022-11-08 15:50:34 -0800

    import success

---
+++ converters.py
@@ -0,0 +1,8 @@
+def kgs_to_lbs(weight):
+    converted_weight = weight / 0.45359237
+    return f"{round(converted_weight)} kgs"
+
+def lbs_to_kgs(weight):
+    converted_weight = weight * 0.45359237
+    return f"{round(converted_weight)} lbs"
+
---
+++ modeule.py
@@ -0,0 +1,8 @@
+#modules are used to help organize your code and make it easy to use/read/edit
+# then your import the module to the app you are writing your code in
+
+import converters
+
+
+print(converters.kgs_to_lbs(70))
+print(converters.lbs_to_kgs(160))
