a2cb7aa652f6c590d59dbb2531d42ab68daeaafe
Joshua Jainga <jjainga@gmail.com>
2022-11-08 09:55:05 -0800

    comments, they are a thing

---
+++ comments.py
@@ -0,0 +1,8 @@
+#using comments to make a comments on the code to help defie what you are doing in your code
+
+# to start a comment use a "#" at the begining of the line
+
+#print "Sky is blue"
+print("Sky is blue")
+# this is a bad comment, try using comments  to define the whys and hows, not the whats
+
