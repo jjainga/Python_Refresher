ac001990a400691a2d5c2471e7d16f2069e34650
Joshua Jainga <jjainga@gmail.com>
2022-11-08 17:38:57 -0800

    automating excel edits

---
+++ transaction_project/excel_automation.py
@@ -0,0 +1,34 @@
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+
+def process_wb(name,save):
+    wb = xl.load_workbook(name)
+    sheet = wb['Sheet1']
+    fixing_price(sheet)
+    create_chart(sheet)
+    wb.save(save)
+
+#starting from 2 to ignore the headers
+def fixing_price(sheet):
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row,3)
+        corrected_price = cell.value * .9
+        corrected_price_cell = sheet.cell(row,4)
+        corrected_price_cell.value = corrected_price
+
+
+#selecting values
+def create_chart(sheet):
+    values = Reference(sheet,
+        min_row=2,
+        max_row=sheet.max_row,
+        min_col=4,
+        max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart,'e2')
+
+
+process_wb('transactions.xlsx','transactions_complete.xlsx')
