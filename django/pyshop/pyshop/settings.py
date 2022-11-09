cf3847f97738c797c01d4721163c8fe17c6f71ef
Joshua Jainga <jjainga@gmail.com>
2022-11-09 08:04:56 -0800

    env variable

--- django/pyshop/pyshop/settings.py
+++ django/pyshop/pyshop/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^s^cebvdk$n1*@vm9+b(148s74uk$x(97l-***3u2f7&t%5*mg'
+SECRET_KEY = os.environ['SERCRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
