684adcb297369c664d990dd31606a91d595b1961
Joshua Jainga <jjainga@gmail.com>
2022-11-09 08:44:46 -0800

    env file

--- django/pyshop/products/views.py
+++ django/pyshop/products/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
+#need to map the url to this function
 def index(request):
     return HttpResponse('Hello World')
--- django/pyshop/pyshop/settings.py
+++ django/pyshop/pyshop/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
-from pathlib import Path
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+from decouple import config
+from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SERCRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
