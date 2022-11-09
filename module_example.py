4b637f3a2e6682d4ef6574d0b778c782bad94cfd
Joshua Jainga <jjainga@gmail.com>
2022-11-08 16:16:56 -0800

    formating packages

--- module_example.py
+++ module_example.py
@@ -1,12 +1,11 @@
 #Our example for using modules
-
-from utils import find_max
+from utility_functions import utils
 
 
 #init a list
 nums = [1,2,3,4,5,6,7,8,9,10]
 
-print(find_max(nums))
+print(utils.find_max(nums))
 
 #max is a already a function in python
 print(max(nums))
