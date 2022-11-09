5ccecaa97ff60474d755d9612290780b73326c7e
Joshua Jainga <jjainga@gmail.com>
2022-11-09 11:09:50 -0800

    added discount

--- django/pyshop/products/admin.py
+++ django/pyshop/products/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Discount_Offers
 
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name','price','stock')
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('coupon_code','discription', 'discount_amount')
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Discount_Offers, DiscountAdmin)
