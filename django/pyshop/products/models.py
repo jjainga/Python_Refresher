f07ef7b933e336fb41a1ac56ec1ff1d6426ab6ec
Joshua Jainga <jjainga@gmail.com>
2022-11-09 09:27:22 -0800

    adding dicount model

--- django/pyshop/products/models.py
+++ django/pyshop/products/models.py
@@ -7,3 +7,7 @@ class Product(models.Model):
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
 
+class Discount_Offers(models.Model):
+    coupon_code = models.CharField(max_length=10)
+    discription = models.CharField(max_length=255)
+    discount_amount = models.FloatField()
